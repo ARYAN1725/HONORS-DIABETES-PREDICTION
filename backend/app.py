03b682ce0b5f468576ea6fa4cd6e5e0743573950
ARYAN1725 <aryanpatil1725@gmail.com>
2025-09-25 15:56:44 +0530

    Added model

---
+++ backend/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load trained model and scaler
+model = joblib.load("model/diabetes_rf_model.pkl")
+scaler = joblib.load("model/scaler.pkl")
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Diabetes Prediction API is running!"})
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # JSON input: {"features": [preg, glucose, bp, skin, insulin, bmi, dpf, age]}
+        data = request.get_json()
+        features = np.array(data["features"]).reshape(1, -1)
+
+        # Scale input
+        features_scaled = scaler.transform(features)
+
+        # Prediction
+        prediction = int(model.predict(features_scaled)[0])
+        probability = float(model.predict_proba(features_scaled)[:,1][0])
+
+        return jsonify({
+            "prediction": prediction,   # 0 = No diabetes, 1 = Diabetes
+            "probability": probability
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
